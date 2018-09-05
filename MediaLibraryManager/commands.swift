c73a287eea2b343705b2ec2f1156c5ef98a0ed84
Paul Crane <pscrane@gmail.com>
2018-09-05 14:42:35 +1200

    added add/edit/del commands for metadata, plus cosmetic style changes

--- MediaLibraryManager/commands.swift
+++ MediaLibraryManager/commands.swift
@@ -13,64 +13,63 @@ import Foundation
 // *add* this file to your project, you can leave it as is.
 
 /// The list of exceptions that can be thrown by the CLI command handlers
-
 enum MMCliError: Error {
-    
+
     /// Thrown if there is something wrong with the input parameters for the command
     case invalidParameters
-    
+
     /// Thrown if there is no result set to work with (and this command depends
     /// on the previous command)
     case missingResultSet
-    
+
     /// Thrown when the command is not understood
     case unknownCommand
-    
+
     /// Thrown if the command has yet to be implemented
     /// - Note: You'll need to implement these to get the CLI working properly
     case unimplementedCommand
-    
+
     case noCommand
-    
+
     // feel free to add more errors as you need them
 }
 
-
 /// This class representes a set of results. It could be extended to include
 /// the command and a history of commands and the results.
-class MMResultSet{
-    
+class MMResultSet {
+
     /// The list of files produced by the command
     fileprivate var results: [MMFile]
-    
+
     /// Constructs a new result set.
     /// - parameter results: the list of files produced by the executed
     /// command, could be empty.
-    init(_ results:[MMFile]){
+    init(_ results: [MMFile]) {
         self.results = results
     }
     /// Constructs a new result set with an empty list.
-    convenience init(){
+    convenience init() {
         self.init([MMFile]())
     }
-    
+
     /// If there are some results to show, enumerate them and print them out.
     /// - note: this enumeration is used to identify the files in subsequent
     /// commands.
-    func show(){
-        guard self.results.count > 0 else{
+    func show() {
+        guard self.results.count > 0 else {
             return
         }
-        for (i,file) in self.results.enumerated() {
+        //swiftlint:disable:next identifier_name
+        for (i, file) in self.results.enumerated() {
             print("\(i): \(file)")
         }
     }
-    
-    func get(index: Int) throws -> MMFile{
+
+    func get(index: Int) throws -> MMFile {
         return self.results[index]
     }
-    
-    func result() -> [MMFile]{
+
+    func result() -> [MMFile] {
         return results
     }
 }
@@ -84,14 +83,12 @@ class MMResultSet{
 func prompt(_ prompt: String, strippingNewline: Bool = true) -> String? {
     // the following terminator specifies *not* to put a newline at the
     // end of the printed line
-    print(prompt, terminator:"")
+    print(prompt, terminator: "")
     return readLine(strippingNewline: strippingNewline)
 }
 
-
-/// This protocol specifies the new 'Command' pattern, and is more
-/// Object Oriented.
-protocol MMCommand{
+/// This protocol specifies the new 'Command' pattern, and is more Object Oriented.
+protocol MMCommand {
     var results: MMResultSet? {get}
     func execute() throws
 }
@@ -118,17 +115,17 @@ protocol MMCommand{
 
 /// Handle unimplemented commands by throwing an exception when trying to
 /// execute this command.
-class UnimplementedCommand: MMCommand{
-    var results: MMResultSet? = nil
-    func execute() throws{
+class UnimplementedCommand: MMCommand {
+    var results: MMResultSet?
+    func execute() throws {
         throw MMCliError.unimplementedCommand
     }
 }
 
 /// Handle the help command.
-class HelpCommand: MMCommand{
-    var results: MMResultSet? = nil
-    func execute() throws{
+class HelpCommand: MMCommand {
+    var results: MMResultSet?
+    func execute() throws {
         print("""
 \thelp                              - this text
 \tload <filename> ...               - load file into the collection
@@ -142,18 +139,18 @@ class HelpCommand: MMCommand{
 \tquit                              - exit the program (without prompts)
 """)
         // for example:
-        
+
         // load foo.json bar.json
         //      from the current directory load both foo.json and bar.json and
         //      merge the results
-        
+
         // list foo bar baz
         //      results in a set of files with metadata containing foo OR bar OR baz
-        
+
         // add 3 foo bar
         //      using the results of the previous list, add foo=bar to the file
         //      at index 3 in the list
-        
+
         // add 3 foo bar baz qux
         //      using the results of the previous list, add foo=bar and baz=qux
         //      to the file at index 3 in the list
@@ -162,47 +159,212 @@ class HelpCommand: MMCommand{
 
 /// Handle the quit command. Exits the program (with exit code 0) without
 /// checking if there is anything to save.
-class QuitCommand : MMCommand{
-    var results: MMResultSet? = nil
-    func execute() throws{
+class QuitCommand: MMCommand {
+    var results: MMResultSet?
+    func execute() throws {
         exit(0)
     }
 }
 
+class AddMetadataCommand: MMCommand {
+    var results: MMResultSet?
 
-class LoadCommand : MMCommand{
-    var results: MMResultSet? = nil
+    var params: [String]
+    var collection: Collection
+    var items: [MMFile]
+
+    init(collection: Collection, items: [MMFile], params: [String]) {
+        // I don't think we need the collection for this part if we were to
+        // use the observer pattern
+        self.params = params
+        self.items = items
+        self.collection = collection
+    }
+
+    func execute() throws {
+        if params.count < 1 {
+            throw MMCliError.invalidParameters
+        }
+
+        guard let index = Int(params.remove(at: 0)) else {
+            throw MMCliError.invalidParameters
+        }
+
+        let file = self.items[index]
+
+        //TODO: pairwise param traversal
+        let keyword = params.remove(at: 0)
+        let value = params.remove(at: 0)
+
+        //swiftlint:disable:next todo
+        //TODO: bad code smell!!
+        //swiftlint:disable:next force_cast
+        (file as! File).add(keyword: keyword, value: value)
+
+        // this is necessary as we've made a copy of the file and modified it
+        collection.replace(file: file)
+
+        //swiftlint:disable:next todo
+        //TODO: this is inefficient
+        collection.reindex()
+    }
+}
+
+class SetMetadataCommand: MMCommand {
+    var results: MMResultSet?
+
+    var params: [String]
+    var collection: Collection
+    var items: [MMFile]
+
+    init(collection: Collection, items: [MMFile], params: [String]) {
+        // I don't think we need the collection for this part if we were to
+        // use the observer pattern
+        self.params = params
+        self.items = items
+        self.collection = collection
+    }
+
+    func execute() throws {
+
+        throw MMCliError.unimplementedCommand
+
+        if params.count < 1 {
+            throw MMCliError.invalidParameters
+        }
+        guard let index = Int(params.remove(at: 0)) else {
+            throw MMCliError.invalidParameters
+        }
+
+        let file = self.items[index]
+
+        //swiftlint:disable:next todo
+        //TODO: pairwise param traversal
+        let keyword = params.remove(at: 0)
+        let value = params.remove(at: 0)
+
+        //swiftlint:disable:next todo
+        //TODO: bad code smell!!
+        //swiftlint:disable:next todo
+        //TODO: actually set the data
+        //swiftlint:disable:next force_cast
+        (file as! File).add(keyword: keyword, value: value)
+
+        // this is necessary as we've made a copy of the file and modified it
+        collection.replace(file: file)
+
+        //swiftlint:disable:next todo
+        //TODO: this is inefficient
+        collection.reindex()
+    }
+}
+
+class DelMetadataCommand: MMCommand {
+    var results: MMResultSet?
+
+    var params: [String]
+    var collection: Collection
+    var items: [MMFile]
+
+    init(collection: Collection, items: [MMFile], params: [String]) {
+        // I don't think we need the collection for this part if we were to
+        // use the observer pattern
+        self.params = params
+        self.items = items
+        self.collection = collection
+    }
+
+    func execute() throws {
+
+        throw MMCliError.unimplementedCommand
+
+        if params.count < 1 {
+            throw MMCliError.invalidParameters
+        }
+        guard let index = Int(params.remove(at: 0)) else {
+            throw MMCliError.invalidParameters
+        }
+
+        var file = self.items[index]
+
+        //swiftlint:disable:next todo
+        //TODO: pairwise param traversal
+        let keyword = params.remove(at: 0)
+        let value = params.remove(at: 0)
+
+        //swiftlint:disable:next todo
+        //TODO: bad code smell -- too much coupling
+        file.remove(metadata: Metadata(keyword: keyword, value: value))
+
+        //swiftlint:disable:next todo
+        //TODO: this is inefficient
+        collection.reindex()
+    }
+}
+
+class LoadCommand: MMCommand {
+    var results: MMResultSet?
     var paths: [String]
     var collection: Collection
-    
-    init(collection: Collection, paths: [String]){
+
+    init(collection: Collection, paths: [String]) {
         self.paths = paths
         self.collection = collection
     }
-    
-    func execute() throws{
-        for path in self.paths{
+
+    func execute() throws {
+        for path in self.paths {
             collection.load(filename: path)
         }
     }
 }
 
-class ListCommand : MMCommand{
-    var results: MMResultSet? = nil
+class ListCommand: MMCommand {
+    var results: MMResultSet?
     var terms: [String]
     var collection: Collection
-    
-    init(collection: Collection, terms: [String]){
+
+    init(collection: Collection, terms: [String]) {
         self.terms = terms
         self.collection = collection
     }
-    
-    func execute() throws{
-        // todo add in other search terms
-        if self.terms.count > 0{
+
+    func execute() throws {
+        //swiftlint:disable:next todo
+        // TODO: add in other search terms
+        // create a dictionary indexed by path and add files to it for each of the terms
+        if self.terms.count > 0 {
             self.results = MMResultSet(self.collection.search(term: self.terms[0]))
         } else {
            self.results = MMResultSet(self.collection.all())
+        }
+    }
+}
+
+class SaveCommand: MMCommand {
+    var results: MMResultSet?
+
+    var items: [MMFile]?
+    var collection: Collection
+    var filename: [String]
+
+    init(collection: Collection, filename: [String], items: [MMFile]?) {
+        self.items = items
+        self.collection = collection
+        self.filename = filename
+    }
+
+    func execute() throws {
+
+        guard self.filename.count == 1 else {
+            throw MMCliError.invalidParameters
+        }
+
+        let filename = self.filename[0]
+        if let items = self.items {
+            self.collection.save(filename: filename, list: items)
+        } else {
+            self.collection.save(filename: filename)
         }
     }
 }
