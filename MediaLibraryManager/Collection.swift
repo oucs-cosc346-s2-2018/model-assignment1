c226e865baf4046e6281cdd8621d56e7e0274dc1
Paul Crane <pscrane@gmail.com>
2018-09-05 14:40:40 +1200

    progress towards add/edit/remove metadata

--- MediaLibraryManager/Collection.swift
+++ MediaLibraryManager/Collection.swift
@@ -8,117 +8,157 @@
 
 import Foundation
 
-class Indexer{
-    var index:[String: [MMFile]]
-    
-    init(){
+class Indexer {
+    var index: [String: [MMFile]]
+
+    init() {
         index = [:]
     }
-    
-    func search(term:String) -> [MMFile]? {
+
+    func search(term: String) -> [MMFile]? {
         return index[term]
     }
-    
-    func add(term: String, file: MMFile){
+
+    func add(files: [MMFile]) {
+        for file in files {
+            self.add(file: file)
+        }
+    }
+
+    func add(file: MMFile) {
+        //swiftlint:disable:next identifier_name
+        for md in file.metadata {
+            self.add(term: md.value, file: file)
+        }
+    }
+
+    func add(term: String, file: MMFile) {
         if index[term] != nil {
             index[term]?.append(file)
-        }else{
+        } else {
             index[term] = [file]
         }
     }
-    func remove(term: String, f: MMFile){
+
+    //swiftlint:disable:next identifier_name
+    func remove(term: String, f: MMFile) {
         // this removes the metadata from the index
         index[term] = index[term]?.filter({$0.filename != f.filename})
     }
 }
 
-extension MMFile{
-    func isRequired(metadata: MMMetadata) -> Bool{
+extension MMFile {
+    func isRequired(metadata: MMMetadata) -> Bool {
         // I don't like this downcasting, but unless the protocol is
         // modified, I can't think of a way around this.
-        if let me = (self as? File){
-            if me.requiredMetadata.contains(metadata.keyword){
+        //swiftlint:disable:next identifier_name
+        if let me = (self as? File) {
+            if me.requiredMetadata.contains(metadata.keyword) {
                 return true
             }
         }
         return false
     }
-    
-    mutating func remove(metadata: MMMetadata){
+
+    func contains(metadata: MMMetadata) -> Bool {
+        //swiftlint:disable:next identifier_name
+        for md in self.metadata {
+            //swiftlint:disable:next todo
+            //TODO: make metadata equatable
+            if md.keyword == metadata.keyword && md.value == metadata.value {
+                return true
+            }
+        }
+        return false
+    }
+
+    mutating func remove(metadata: MMMetadata) {
         self.metadata = self.metadata.filter({$0.keyword != metadata.keyword && $0.value != metadata.value})
     }
 }
 
 // we'll do the bare minimum to make this work
-class Collection: MMCollection  {
-    
-    var importers:[String:MMFileImport]
-    var exporters:[String:MMFileExport]
-    
-    var files: [MMFile]
-    var index: Indexer
-    
-    init(){
+class Collection: MMCollection {
+    private var importers: [String: MMFileImport]
+    private var exporters: [String: MMFileExport]
+    private var files: [MMFile]
+    private var index: Indexer
+
+    init() {
         files = []
-        importers = ["json":Importer()]
-        exporters = [:]
+        // this allows us to add additional importers/exporters for
+        // different serialisation types
+        importers = ["json": Importer()]
+        exporters = ["json": Exporter()]
         index = Indexer()
     }
-    
+
+    // brute force reindexing of the library
+    func reindex() {
+        self.index = Indexer()
+        self.index.add(files: self.files)
+    }
+
     // this may need to update the index
     func add(file: MMFile) {
         self.files.append(file)
-        for md in file.metadata{
-            self.add(metadata: md, file: file)
-        }
+        self.index.add(file: file)
     }
-    
+
     // this is part of the index's functionality
     func add(metadata: MMMetadata, file: MMFile) {
         index.add(term: metadata.value, file: file)
     }
-    
+
+    func replace(file: MMFile) {
+        // this is done specifically to set the item at that position of the list.
+        // if we use a for x in y style loop, then we'd end up just manipulating a copy?
+        //swiftlint:disable:next identifier_name
+        for i in 0..<self.files.count where self.files[i].filename == file.filename {
+            self.files[i] = file
+            break
+        }
+    }
+
     // this requires both file and index
     func remove(metadata: MMMetadata) {
-        if let list = index.search(term: metadata.value){
+        if let list = index.search(term: metadata.value) {
             for var file in list {
-                if !file.isRequired(metadata: metadata){
+                if !file.isRequired(metadata: metadata) {
                     index.remove(term: metadata.value, f: file)
                     file.remove(metadata: metadata)
                 }
             }
         }
     }
-    
-    // this is part of the index
+
     func search(term: String) -> [MMFile] {
-        if let result = index.search(term: term){
+        if let result = index.search(term: term) {
             return result
         }
         return []
     }
-    
-    // this is part of the index
+
     func search(item: MMMetadata) -> [MMFile] {
-        print("that search is not implemented")
-        return []
+        // we first reduce the number of files that we need to look through
+        let result = self.search(term: item.value)
+        // and then we look see if the metadata is contained in the file
+        return result.filter({$0.contains(metadata: item)})
     }
-    
+
     func all() -> [MMFile] {
         return self.files
     }
-    
-    var description: String{
+
+    var description: String {
         return "Collection contains \(self.files.count) files."
     }
-    
-    // this should work
-    func load(filename: String){
-        do{
-            // find file type of the filename and lookup value in the dictionary
-            if let importer = importers["json"]{
+
+    func load(filename: String) {
+        do {
+            if let importer = importers["json"] {
                 let files = try importer.read(filename: filename)
-                for file in files{
+                for file in files {
                     self.add(file: file)
                 }
             }
@@ -128,8 +168,20 @@ class Collection: MMCollection  {
             print("unknown import exception")
         }
     }
-    
-    func save(filename: String){
-        print("not implemented!")
+
+    func save(filename: String, list: [MMFile]) {
+        do {
+            if let exporter = exporters["json"] {
+                try exporter.write(filename: filename, items: list)
+            }
+        } catch where error is MMExportError {
+            print(error)
+        } catch {
+            print("unknown export exception")
+        }
+    }
+
+    func save(filename: String) {
+        self.save(filename: filename, list: self.files)
     }
 }
